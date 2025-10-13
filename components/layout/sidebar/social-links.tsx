267e5f69b83e9f7e14d3dcfd6b6b9de0f575deb5
AhmedReda958 <areda.gad@gmail.com>
2025-10-13 18:28:12 +0300

    Update social media links in sidebar component for accuracy; replace outdated URLs with current profiles for improved user engagement.

--- components/layout/sidebar/social-links.tsx
+++ components/layout/sidebar/social-links.tsx
@@ -6,20 +6,19 @@ import { FaXTwitter, FaSnapchat } from "react-icons/fa6";
 const socialMediaLinks = [
   {
     icon: FaXTwitter,
-    href: "https://www.twitter.com/inspectex",
+    href: "https://twitter.com/Inspectex_sa",
   },
   {
     icon: FaSnapchat,
-    href: "https://www.snapchat.com/inspectex",
+    href: "https://www.snapchat.com/add/inspectex_sa?sender_web_id=8d642428-1ad2-46f9-bde9-0128354ce7dd&device_type=desktop&is_copy_url=true",
   },
-
   {
     icon: FaInstagram,
-    href: "https://www.instagram.com/inspectex",
+    href: "https://www.instagram.com/inspectex_sa/",
   },
   {
     icon: FaFacebookF,
-    href: "https://www.facebook.com/inspectex",
+    href: "https://www.facebook.com/profile.php?id=61551634089888&mibextid=kFxxJD",
   },
 ];
 
